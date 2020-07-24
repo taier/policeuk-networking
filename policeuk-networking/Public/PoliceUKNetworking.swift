2d323027817cda127790c4c690de7240db98a42f
Deniss kaibagarovs <deniss.kaibagarovs@gmail.com>
2020-07-24 19:27:36 +0100

    add test function

---
+++ policeuk-networking/Public/PoliceUKNetworking.swift
@@ -0,0 +1,15 @@
+//
+//  PoliceUKNetworking.swift
+//  policeuk-networking
+//
+//  Created by Deniss Kaibagarovs on 24/07/2020.
+//  Copyright © 2020 Deniss Kaibagarovs. All rights reserved.
+//
+
+import Foundation
+
+public class PoliceUKNetworking {
+  static func getNumberOfCrimes() -> Int {
+    return 10
+  }
+}
